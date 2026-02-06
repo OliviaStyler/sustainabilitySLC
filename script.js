48cae69b7bab08a9fde560133044cdb514746ac9
Olivia Styler <olivia.styler@utah.edu>
2026-02-06 14:48:50 -0700

    improving popup window sizing, scrolling and button configurations

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let popup = document.getElementById("popup");
 
 function openPopup(){
   popup.classList.add("openPopup");
+  document.body.classList.add("popup-open");
+
 }
 
 function closePopup(){
   popup.classList.remove("openPopup");
+  document.body.classList.remove("popup-open");
+
 
 }
